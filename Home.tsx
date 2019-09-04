7d8ee3e43a80b403746f5a4c207bcfe11ee3752a
Harrison Leach <HarrisonL7190@gmail.com>
2019-09-05 04:58:27 +1200

    Add basic rendering of data

--- Home.tsx
+++ Home.tsx
@@ -1,18 +1,20 @@
 import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, FlatList } from "react-native";
+import { List, Divider } from "react-native-paper";
+import { DummyData } from "./DummyData";
 
 const Home = ({ history }) => {
     return (
         <View style={styles.container}>
-            <View>
+            <View style={styles.graph}>
                 <Text>Graph</Text>
             </View>
-            <View>
-                <Button
-                    title="Workout"
-                    onPress={() => history.push("/Workout")}
-                />
-            </View>
+            <FlatList
+                renderItem={({ item }) => <List.Item title={item.title} />}
+                keyExtractor={item => item.title}
+                ItemSeparatorComponent={Divider}
+                data={DummyData}
+            />
         </View>
     );
 };
@@ -23,6 +25,12 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         flexDirection: "column",
+        backgroundColor: "#fff",
+        alignItems: "stretch",
+        justifyContent: "center"
+    },
+    graph: {
+        flex: 1,
         backgroundColor: "#fff",
         alignItems: "center",
         justifyContent: "center"
