c7e38c431698b03a2e15a52a202250adfda0aefd
Harrison Leach <HarrisonL7190@gmail.com>
2019-09-04 21:08:03 +1200

    Add button to get back to home page from workout page

--- Workout.tsx
+++ Workout.tsx
@@ -1,14 +1,16 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
-import Timer from './Timer'
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import Timer from "./Timer";
+import { RouteComponentProps } from "react-router-native";
 
-interface IWorkoutProps {
-    workout: Workout
+interface IWorkoutProps extends RouteComponentProps {
+    workout: Workout;
 }
 
-const Workout = ({ workout }: IWorkoutProps) => {
-    const [currentExercise, setCurrentExercise] = useState<Exercise>(workout.exercises[0]);
+const Workout = ({ history, workout }: IWorkoutProps) => {
+    const [currentExercise, setCurrentExercise] = useState<Exercise>(
+        workout.exercises[0]
+    );
     const [currentSet, setCurrentSet] = useState(1);
 
     return (
@@ -18,27 +20,30 @@ const Workout = ({ workout }: IWorkoutProps) => {
             </View>
             <View style={styles.workoutInfoContainer}>
                 <Text>{currentExercise.title}</Text>
-                <Text>Set: {currentSet} / {currentExercise.sets}</Text>
+                <Text>
+                    Set: {currentSet} / {currentExercise.sets}
+                </Text>
                 <Text>Weight: {currentExercise.weightInKg} Kg</Text>
                 <Text>Reps: {currentExercise.repetitions}</Text>
+                <Button onPress={() => history.push("/")} title="Go Back" />
             </View>
         </React.Fragment>
     );
-}
+};
 
 const styles = StyleSheet.create({
     workoutInfoContainer: {
         flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center"
     },
     timerContainer: {
         flex: 2,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center"
+    }
 });
 
 export default Workout;
