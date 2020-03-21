b3dc099fe68c03c7911f3d2890279e6b5c3968a8
Harrison Leach <HarrisonL7190@gmail.com>
2020-03-21 19:52:02 +1300

    Add ability to start workout

--- src/views/Routine/RoutinePreviewCard.tsx
+++ src/views/Routine/RoutinePreviewCard.tsx
@@ -1,6 +1,6 @@
 import { Routine } from "../../types/WorkoutTypes";
-import { Surface, Theme, Title, Paragraph, Headline } from "react-native-paper";
-import { StyleSheet, Text } from "react-native";
+import { Surface, Theme, Title, Paragraph, Headline, Divider, IconButton } from "react-native-paper";
+import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 
 export interface IRoutinePreviewCardProps {
@@ -10,16 +10,21 @@ export interface IRoutinePreviewCardProps {
 }
 
 const RoutinePreviewCard: React.FC<IRoutinePreviewCardProps> = ({ theme, onPress, routine }) => {
-  const description : string = routine.exercises.items.map((item) => ` ${item.name}`).toString();
-  
-    return (
+  const description: string = routine.exercises.items.map(item => ` ${item.name} ${item.sets} x ${item.repetitions} x ${item.weightInKg} kg`).join(",\n");
+
+  return (
     <Surface style={styles.surface}>
-      <Text style={{ ...styles.header, color: theme.colors.text}} numberOfLines={1} ellipsizeMode="tail">
-        {routine.name}
-      </Text>
-      <Text style={{ ...styles.description, color: theme.colors.text }} numberOfLines={3} ellipsizeMode="tail">
-        {description}
-      </Text>
+      <View style={styles.textContainer}>
+        <Text style={{ ...styles.header, color: theme.colors.text }} numberOfLines={1} ellipsizeMode="tail">
+          {routine.name}
+        </Text>
+        <Text style={{ ...styles.description, color: "grey" }} numberOfLines={3} ellipsizeMode="tail">
+          {description}
+        </Text>
+      </View>
+      <View style={styles.start}>
+        <IconButton icon="play" animated={true} size={40} color={theme.colors.primary} onPress={onPress} />
+      </View>
     </Surface>
   );
 };
@@ -31,13 +36,21 @@ const styles = StyleSheet.create({
     fontSize: 18
   },
   description: {
+    marginTop: 6,
     fontSize: 12
   },
-  surface: {
-    padding: 8,
-    margin: 12,
+  textContainer: {
     alignItems: "flex-start",
-    justifyContent: "center",
-    elevation: 4
+    justifyContent: "center"
+  },
+  start: {
+    alignSelf: "center"
+  },
+  surface: {
+    padding: 12,
+    margin: 12,
+    elevation: 4,
+    flexDirection: "row",
+    justifyContent: "space-between"
   }
 });
