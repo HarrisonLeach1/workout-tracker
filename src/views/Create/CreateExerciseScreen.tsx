e7a0510568d188729055fc3925990d1f7b3b1463
Harrison Leach <HarrisonL7190@gmail.com>
2020-03-24 14:52:18 +1300

    Add form validation to exercise creation
    Added yup library because of easy integration with formik

--- src/views/Create/CreateExerciseScreen.tsx
+++ src/views/Create/CreateExerciseScreen.tsx
@@ -1,165 +1,161 @@
 import React, { useContext } from "react";
-import {
-    StyleSheet,
-    KeyboardAvoidingView,
-    ScrollView,
-    View
-} from "react-native";
-import {
-    TextInput,
-    Button,
-    Appbar,
-    withTheme,
-    Theme
-} from "react-native-paper";
-import { Formik, FormikProps, FormikActions } from "formik";
-import {
-    CreateRoutineContext,
-    CreateRoutineContextProps
-} from "../../contexts/RoutineContext";
+import { StyleSheet, KeyboardAvoidingView, ScrollView, View } from "react-native";
+import { TextInput, Button, Appbar, withTheme, Theme, HelperText } from "react-native-paper";
+import { Formik, FormikProps, FormikActions, ErrorMessage } from "formik";
+import { CreateRoutineContext, CreateRoutineContextProps } from "../../contexts/RoutineContext";
 import { CreateExerciseInput } from "../../API";
 import { RouteComponentProps } from "react-router";
+import * as Yup from "yup";
 
 interface ICreateExerciseScreenProps extends RouteComponentProps {
-    theme: Theme;
+  theme: Theme;
 }
 
 interface ExerciseFormValues {
-    name: string;
-    sets: string;
-    repetitions: string;
-    weightInKg: string;
+  name: string;
+  sets: string;
+  repetitions: string;
+  weightInKg: string;
 }
 
-const CreateExerciseScreen: React.FC<ICreateExerciseScreenProps> = ({
-    history,
-    theme
-}: ICreateExerciseScreenProps) => {
-    const { routine, setRoutine } = useContext<CreateRoutineContextProps>(
-        CreateRoutineContext
-    );
+const CreateExerciseScreen: React.FC<ICreateExerciseScreenProps> = ({ history, theme }: ICreateExerciseScreenProps) => {
+  const { routine, setRoutine } = useContext<CreateRoutineContextProps>(CreateRoutineContext);
 
-    const handleSubmit = (
-        values: ExerciseFormValues,
-        actions: FormikActions<ExerciseFormValues>
-    ) => {
-        const exercise: CreateExerciseInput = {
-            name: values.name,
-            sets: +values.sets,
-            repetitions: +values.repetitions,
-            weightInKg: +values.weightInKg,
-            exerciseRoutineId: null
-        };
+  const numberValidationSchema = Yup.number()
+    .typeError("Must be a number")
+    .required("Required")
+    .positive("Must be positive")
+    .test("len", "Max length exceeded", val => val && val.toString().length <= 6);
 
-        setRoutine(prev => {
-            prev.createExercisesInput.push(exercise);
-            return prev;
-        });
+  const validationSchema = Yup.object({
+    name: Yup.string().required("Exercise name is required"),
+    sets: numberValidationSchema,
+    repetitions: numberValidationSchema,
+    weightInKg: numberValidationSchema
+  });
 
-        history.goBack();
+  const handleSubmit = (values: ExerciseFormValues, actions: FormikActions<ExerciseFormValues>) => {
+    const exercise: CreateExerciseInput = {
+      name: values.name,
+      sets: +values.sets,
+      repetitions: +values.repetitions,
+      weightInKg: +values.weightInKg,
+      exerciseRoutineId: null
     };
 
-    return (
-        <View style={styles.screen}>
-            <Appbar.Header>
-                <Appbar.Action icon="delete" onPress={() => history.goBack()} />
-                <Appbar.Content title="Create Exercise" />
-            </Appbar.Header>
+    setRoutine(prev => {
+      prev.createExercisesInput.push(exercise);
+      return prev;
+    });
 
-            <KeyboardAvoidingView
-                style={styles.wrapper}
-                behavior="padding"
-                keyboardVerticalOffset={0}
-            >
-                <ScrollView
-                    style={styles.container}
-                    keyboardShouldPersistTaps={"always"}
-                    removeClippedSubviews={false}
-                >
-                    <Formik
-                        initialValues={{
-                            name: "",
-                            sets: "3",
-                            repetitions: "8",
-                            weightInKg: "20"
-                        }}
-                        onSubmit={handleSubmit}
-                    >
-                        {(props: FormikProps<ExerciseFormValues>) => (
-                            <View>
-                                <TextInput
-                                    style={styles.inputContainerStyle}
-                                    label="Exercise Name"
-                                    placeholder="Type something"
-                                    onChangeText={props.handleChange("name")}
-                                    onBlur={props.handleBlur("name")}
-                                    value={props.values.name}
-                                />
+    history.goBack();
+  };
 
-                                <TextInput
-                                    style={styles.inputContainerStyle}
-                                    label="Number of Sets"
-                                    placeholder="Type something"
-                                    onChangeText={props.handleChange("sets")}
-                                    onBlur={props.handleBlur("sets")}
-                                    value={props.values.sets}
-                                />
+  return (
+    <View style={styles.screen}>
+      <Appbar.Header>
+        <Appbar.Action icon="delete" onPress={() => history.goBack()} />
+        <Appbar.Content title="Create Exercise" />
+      </Appbar.Header>
 
-                                <TextInput
-                                    style={styles.inputContainerStyle}
-                                    label="Goal Repetitions"
-                                    placeholder="Type something"
-                                    onChangeText={props.handleChange(
-                                        "repetitions"
-                                    )}
-                                    onBlur={props.handleBlur("repetitions")}
-                                    value={props.values.repetitions}
-                                />
-
-                                <TextInput
-                                    style={styles.inputContainerStyle}
-                                    label="Goal Weight (Kg)"
-                                    placeholder="Type something"
-                                    onChangeText={props.handleChange(
-                                        "weightInKg"
-                                    )}
-                                    onBlur={props.handleBlur("weightInKg")}
-                                    value={props.values.weightInKg}
-                                />
-                                <Button
-                                    mode="contained"
-                                    /*TODO: Cast is needed here due to existing bug with Formik Types with React Native: 
+      <KeyboardAvoidingView style={styles.wrapper} behavior="padding" keyboardVerticalOffset={0}>
+        <ScrollView style={styles.container} keyboardShouldPersistTaps={"always"} removeClippedSubviews={false}>
+          <Formik
+            initialValues={{
+              name: "",
+              sets: "",
+              repetitions: "",
+              weightInKg: ""
+            }}
+            onSubmit={handleSubmit}
+            validationSchema={validationSchema}
+          >
+            {({ handleChange, handleBlur, errors, touched, values, handleSubmit }: FormikProps<ExerciseFormValues>) => (
+              <View>
+                <TextInput
+                  style={styles.inputContainerStyle}
+                  label="Exercise Name"
+                  placeholder=""
+                  onChangeText={handleChange("name")}
+                  onBlur={handleBlur("name")}
+                  value={values.name}
+                  error={errors.name && touched.name}
+                />
+                <HelperText type="error" visible={errors.name && touched.name}>
+                  {touched.name && errors.name}
+                </HelperText>
+                {console.log("errors: " + JSON.stringify(errors))}
+                <TextInput
+                  style={styles.inputContainerStyle}
+                  label="Number of Sets"
+                  placeholder=""
+                  onChangeText={handleChange("sets")}
+                  onBlur={handleBlur("sets")}
+                  value={values.sets}
+                  error={errors.sets && touched.sets}
+                />
+                <HelperText type="error" visible={touched.sets && errors.sets}>
+                  {touched.sets && errors.sets}
+                </HelperText>
+                <TextInput
+                  style={styles.inputContainerStyle}
+                  label="Goal Weight (Kg)"
+                  placeholder=""
+                  onChangeText={handleChange("weightInKg")}
+                  onBlur={handleBlur("weightInKg")}
+                  value={values.weightInKg}
+                  error={errors.weightInKg && touched.weightInKg}
+                />
+                <HelperText type="error" visible={errors.weightInKg && touched.weightInKg}>
+                  {touched.weightInKg && errors.weightInKg}
+                </HelperText>
+                <TextInput
+                  style={styles.inputContainerStyle}
+                  label="Goal Repetitions"
+                  placeholder=""
+                  onChangeText={handleChange("repetitions")}
+                  onBlur={handleBlur("repetitions")}
+                  value={values.repetitions}
+                  error={errors.repetitions && touched.repetitions}
+                />
+                <HelperText type="error" visible={errors.repetitions && touched.repetitions}>
+                  {touched.repetitions && errors.repetitions}
+                </HelperText>
+                <Button
+                  style={{ marginHorizontal: 25, marginTop: 15, marginBottom: 20 }}
+                  mode="contained"
+                  /*TODO: Cast is needed here due to existing bug with Formik Types with React Native: 
                                 https://github.com/jaredpalmer/formik/issues/376 */
-                                    onPress={props.handleSubmit as any}
-                                >
-                                    Submit
-                                </Button>
-                            </View>
-                        )}
-                    </Formik>
-                </ScrollView>
-            </KeyboardAvoidingView>
-        </View>
-    );
+                  onPress={handleSubmit as any}
+                >
+                  Create Exercise
+                </Button>
+              </View>
+            )}
+          </Formik>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </View>
+  );
 };
 
-const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        flexDirection: "column",
-        justifyContent: "flex-start"
-    },
-    container: {
-        flex: 1,
-        padding: 8
-    },
-    wrapper: {
-        flex: 1,
-        marginTop: 16
-    },
-    inputContainerStyle: {
-        margin: 8
-    }
-});
-
 export default withTheme(CreateExerciseScreen);
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "flex-start"
+  },
+  container: {
+    flex: 1,
+    padding: 8
+  },
+  wrapper: {
+    flex: 1,
+    marginTop: 16
+  },
+  inputContainerStyle: {
+    margin: 8
+  }
+});
